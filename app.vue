e84266ce0726ac56147dd3b7401b7a16e35bfaa5
Emir Uluçay <74816727+emirulucay@users.noreply.github.com>
2023-07-07 09:37:00 +0300

    feat: layout added and add inter font (#19)

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: 'use-credentials',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap',
+    },
+  ],
+  title: 'productdevbook',
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <div class="text-white text-3xl">
+      Blendersheik
+    </div>
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-woodsmoke-950 w-full h-full">
+    <slot />
+  </div>
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  @apply w-full h-full;
+}
+</style>
