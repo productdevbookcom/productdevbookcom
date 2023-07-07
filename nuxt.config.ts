f8f643f36a53e55af0eef9714d6407f8c8db0ca7
Emir Uluçay <74816727+emirulucay@users.noreply.github.com>
2023-07-07 08:54:11 +0300

    feat: tailwindcss (#18)
    * chore: init
    
    * fix: devtools version
    
    * chore: tailwind installed
    
    * fix: lint
    
    * refactor: nuxtTailwind css module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+
 })
