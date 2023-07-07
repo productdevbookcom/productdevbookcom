8ddc39b76bb1c417db77e6009f882dd90658a378
emirulucay <eux202@gmail.com>
2023-07-07 14:53:34 +0300

    fix: mobile responsive

--- components/Button.vue
+++ components/Button.vue
@@ -11,7 +11,7 @@ defineProps<ButtonProps>()
 
 <template>
   <a
-    :target="blank === true ? '_blank' : ''" :class="[priority === 'primary' ? 'primary-btn hover:bg-royalblue-500 hover:border-royalblue-500' : 'secondary-btn hover:bg-codgray-900']"
+    :target="blank === true ? '_blank' : ''" :class="[priority === 'primary' ? 'primary-btn hover:bg-royalblue-500 hover:border-royalblue-500 px-3 py-2 md:px-4 md:py-[10px]' : 'secondary-btn hover:bg-codgray-900 px-3 py-2 md:px-4 md:py-[10px]']"
     :href="link"
   >{{
     name }}</a>
@@ -19,11 +19,11 @@ defineProps<ButtonProps>()
 
 <style lang="postcss">
 .primary-btn {
-  @apply bg-royalblue-600 px-4 py-2 rounded-full border-royalblue-600 border text-white transition duration-300;
+  @apply bg-royalblue-600 rounded-full border-royalblue-600 border text-white transition duration-300;
 }
 
 .secondary-btn {
-  @apply bg-codgray-950 px-4 py-2 rounded-full border-codgray-900 border text-white transition duration-300;
+  @apply bg-codgray-950 rounded-full border-codgray-900 border text-white transition duration-300;
 
 }
 </style>
