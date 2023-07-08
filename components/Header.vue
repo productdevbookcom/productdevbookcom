f483042edab6d98d47c858b942d681f5796e3187
emirulucay <eux202@gmail.com>
2023-07-08 11:55:00 +0300

    feat: header component burger menu

--- components/Header.vue
+++ components/Header.vue
@@ -38,18 +38,37 @@ function Toggle() {
         <div class="w-[22px] h-[1px] bg-white transition duration-300" :class="{ 'rotate-45 translate-y-[1px]': isOpen }" />
         <div class="w-[22px] h-[1px] bg-white transition duration-300" :class="{ '-rotate-45 ': isOpen }" />
       </div>
-      <div class="absolute top-[61px] left-0 translate-x-[200%] w-full h-screen  transition duration-300" :class="{ '!translate-x-[0%]': isOpen }">
-        <ul class="flex flex-col gap-3 px-6 mt-8">
-          <li>
-            <a href="#" class="text-[#818181] text-4xl font-semibold hover:text-white transition duration-300">oku</a>
-          </li>
-          <li>
-            <a href="#" class="text-[#818181] text-4xl  font-semibold hover:text-white transition duration-300">productdevbook</a>
-          </li>
-          <li>
-            <a href="#" class="text-[#818181] text-4xl  font-semibold hover:text-white transition duration-300">huntersofbook</a>
-          </li>
-        </ul>
+      <div class="fixed top-[61px] left-0 translate-x-[200%] w-full h-screen  transition duration-300 px-6" :class="{ '!translate-x-[0%]': isOpen }">
+        <div class="flex flex-col gap-8">
+          <ul class="flex flex-col gap-3 mt-8">
+            <li>
+              <a href="#" class="text-[#818181] text-3xl font-medium hover:text-white transition duration-300">oku</a>
+            </li>
+            <li>
+              <a href="#" class="text-[#818181] text-3xl  font-medium hover:text-white transition duration-300">productdevbook</a>
+            </li>
+            <li>
+              <a href="#" class="text-[#818181] text-3xl  font-medium hover:text-white transition duration-300">huntersofbook</a>
+            </li>
+          </ul>
+          <ul class="flex items-center gap-2">
+            <li>
+              <SocialMedia link="https://github.com/productdevbookcom">
+                <span class="i-fa-brands-github text-white w-5 h-5" />
+              </SocialMedia>
+            </li>
+            <li>
+              <SocialMedia link="https://twitter.com/productdevbook">
+                <span class="i-fa-brands-twitter text-white w-5 h-5" />
+              </SocialMedia>
+            </li>
+            <li>
+              <SocialMedia link="https://chat.productdevbook.com">
+                <span class="i-fa-brands-discord text-white w-5 h-5" />
+              </SocialMedia>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
