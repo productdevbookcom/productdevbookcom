a50727e46f4285768ffe81c212b4ca04a22d35ac
Emir Uluçay <74816727+emirulucay@users.noreply.github.com>
2023-07-08 16:07:45 +0300

    feat: add newsletter component (#26)
    * feat: add newsletter component
    
    * fix: ph icon package uninstall

---
+++ components/Newsletter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col gap-2 items-center">
+    <h6 class="text-white text-2xl md:text-[32px] font-medium text-center">
+      Subscribe to Newsletter
+    </h6>
+    <p class="text-[#979797] leading-normal text-sm md:text-base max-w-[320px] text-center">
+      Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+    </p>
+    <div class="flex items-center gap-2">
+      <form action="" class="flex gap-2 mt-2">
+        <input class="p-2 text-white bg-[#1d1d1d] max-w-[220px] sm:max-w-none rounded-[4px] hover:bg-[#1b1b1b] placeholder:text-[#868686] focus:outline-royalblue-300 focus:outline focus:outline-[1px]" placeholder="Enter email" type="email">
+        <button class="bg-royalblue-600 py-2 text-white px-4 rounded-[4px] hover:bg-opacity-80 transition duration-300">
+          Send
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
